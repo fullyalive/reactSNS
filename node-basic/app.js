8d6b370e946c8dba7d84ef9c4a6559ab19a8f117
fullyalive <wogud1202@gmail.com>
2019-08-25 20:24:42 +0900

    Theory | syncronous & asyncronous programming

--- node-basic/app.js
+++ node-basic/app.js
@@ -9,4 +9,16 @@
 const fs = require("fs");
 const fileName = "target.txt";
 
-fs.watch(fileName, () => console.log(`File changed!`));
+// syncronous 
+const data = fs.readFileSync(fileName)
+console.log(data.toString());
+
+// asyncronous
+fs.readFile(fileName, (err, data) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(data.toString());
+});
+
+console.log("Node js async programming ... ?")
