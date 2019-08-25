3f47cc6616f4b221e6a92133323cc42dbf7d6d9c
fullyalive <wogud1202@gmail.com>
2019-08-25 16:25:54 +0900

    Theory | Object destructuring

--- node-basic/app.js
+++ node-basic/app.js
@@ -1,4 +1,9 @@
-const helpers = require('./helpers');
+// const helpers = require('./helpers');
+// const total = helpers.sum(10, 200);
 
-const total = helpers.sum(10, 200);
+const { sum, add } = require('./helpers');
+const total = sum(10, 200);
+const total2 = add(200, 10);
+
 console.log("Total : ", total)
+console.log("Total2 : ", total2);
--- node-basic/helpers.js
+++ node-basic/helpers.js
@@ -6,11 +6,6 @@
 //   return a + b;
 // };
 
-const sum = (a, b) => a + b; // only 1 line
-
-const plusOne = a => a + 1; // one parameter;
-
-module.exports = {
-  sum,
-  plusOne
-};
+exports.sum = (a, b) => a + b; // only 1 line
+exports.add = (a, b) => a + b;
+exports.plusOne = a => a + 1; // one parameter;
