5f49d557d1fcf8e36ffbb5301fe198d9a6f6725b
fullyalive <wogud1202@gmail.com>
2019-08-25 16:21:48 +0900

    Theory | Arrow Functions

--- node-basic/helpers.js
+++ node-basic/helpers.js
@@ -1,7 +1,16 @@
-function sum(a, b) {
-  return a + b;
-}
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// const sum = (a, b) => {
+//   return a + b;
+// };
+
+const sum = (a, b) => a + b; // only 1 line
+
+const plusOne = a => a + 1; // one parameter;
 
 module.exports = {
-  sum
+  sum,
+  plusOne
 };
