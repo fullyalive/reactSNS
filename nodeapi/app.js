dcf050db57ac0ce409689c041213f0afd2332c7e
fullyalive <wogud1202@gmail.com>
2019-08-27 22:24:00 +0900

    nodeAPI | about middlewares

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
-
+const morgan = require("morgan");
+const myOwnMiddleware =(req, res, next) => {
+    console.log("middleware applied!!!")
+    next();
+}
 // bring in routes
 const postRoutes = require("./routes/post");
 
+// middleware
+app.use(myOwnMiddleware);
+app.use(morgan("dev"));
 app.get("/", postRoutes.getPosts);
 
 const port = 8888;
