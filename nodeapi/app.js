2efa45f181ad020445de5b36e8a92fbe21d7ddc0
fullyalive <wogud1202@gmail.com>
2019-08-27 22:14:14 +0900

    nodeAPI | Seperating routes

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World from node js");
-});
+// bring in routes
+const postRoutes = require("./routes/post");
 
-const port = 8080;
+app.get("/", postRoutes.getPosts);
+
+const port = 8888;
 app.listen(port, () => {
   console.log(`✅ A Node JS API is listening on port: ${port}`);
 });
