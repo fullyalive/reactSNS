49c01f4c9309803eda3d86ea4cfe94bc919c0a2c
fullyalive <wogud1202@gmail.com>
2019-08-27 23:00:18 +0900

    nodeAPI | Using Controllers

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -1,17 +1,13 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-const myOwnMiddleware =(req, res, next) => {
-    console.log("middleware applied!!!")
-    next();
-}
+
 // bring in routes
 const postRoutes = require("./routes/post");
 
 // middleware
-app.use(myOwnMiddleware);
 app.use(morgan("dev"));
-app.get("/", postRoutes.getPosts);
+app.use("/", postRoutes);
 
 const port = 8888;
 app.listen(port, () => {
