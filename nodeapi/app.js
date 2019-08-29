e3e89e1ab401b343a550498264d0be2d0dbfbb95
fullyalive <wogud1202@gmail.com>
2019-08-29 23:58:37 +0900

    nodeAPI | connecting to mongoDB

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const morgan = require("morgan");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+// db
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("✅ DB Connected"));
+const PORT = process.env.PORT || 8888;
 
 // bring in routes
 const postRoutes = require("./routes/post");
@@ -9,7 +19,6 @@ const postRoutes = require("./routes/post");
 app.use(morgan("dev"));
 app.use("/", postRoutes);
 
-const port = 8888;
-app.listen(port, () => {
-  console.log(`✅ A Node JS API is listening on port: ${port}`);
+app.listen(PORT, () => {
+  console.log(`✅ A Node JS API is listening on port: ${PORT}`);
 });
