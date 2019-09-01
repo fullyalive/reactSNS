3c04c997b5617e5af3ee31fa946ecf2cb4d36bc3
fullyalive <wogud1202@gmail.com>
2019-09-01 14:44:42 +0900

    nodeAPI | User signin validation and error messaging

--- nodeapi/validator/index.js
+++ nodeapi/validator/index.js
@@ -22,3 +22,30 @@ exports.createPostValidator = (req, res, next) => {
   // proceed to next middleware
   next();
 };
+
+exports.userSignupValidator = (req, res, next) => {
+  // name is not null and between 4-16 characters
+  req.check("name", "이름을 입력해주세요.").notEmpty();
+
+  // email is not null, valid and normalized
+  req
+    .check("email", "이메일 주소를 입력해주세요.")
+    .notEmpty()
+    .matches(/.+\@.+\..+/)
+    .withMessage("이메일 주소는 @를 포함해야 합니다.");
+  // check for password
+  req.check("password", "비밀번호를 입력해주세요.").notEmpty();
+  req
+    .check("password")
+    .isLength({ min: 6 })
+    .withMessage("비밀번호는 6글자 이상이어야 합니다.");
+  // .matches(/\d/)
+  // .withMessage("비밀번호에 하나 이상의 숫자가 들어가야 합니다.")
+  // check for errors
+  const errors = req.validationErrors();
+  // if error shows the first one as they happen
+  if (errors) {
+    const firstError = errors.map(error => error.msg)[0];
+    return res.status(400).json({ error: firstError });
+  }
+};
