b4f6f5249f1aae895bee9f7c7e577e92bfe356bc
fullyalive <wogud1202@gmail.com>
2019-09-02 00:06:31 +0900

    nodeAPI | Showing all users

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -18,6 +18,7 @@ const PORT = process.env.PORT || 8888;
 // bring in routes
 const postRoutes = require("./routes/post");
 const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
 
 // middleware
 app.use(morgan("dev"));
@@ -26,6 +27,7 @@ app.use(cookieParser());
 app.use(expressValidator());
 app.use("/", postRoutes);
 app.use("/", authRoutes);
+app.use("/", userRoutes);
 app.use(function(err, req, res, next) {
   if (err.name === "UnauthorizedError") {
     res.status(401).json({ error: "로그아웃되었습니다. 다시 로그인해주세요." });
