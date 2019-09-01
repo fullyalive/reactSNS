2d5915650fe8a516feb6efb20ff04dcf1259863a
fullyalive <wogud1202@gmail.com>
2019-09-01 19:47:45 +0900

    nodeAPI | User signin flow

--- nodeapi/controllers/auth.js
+++ nodeapi/controllers/auth.js
@@ -1,4 +1,6 @@
+const jwt = require("jsonwebtoken");
 const User = require("../models/user");
+require("dotenv").config();
 
 exports.signup = async (req, res) => {
   const userExists = await User.findOne({ email: req.body.email });
@@ -10,4 +12,14 @@ exports.signup = async (req, res) => {
   const user = await new User(req.body);
   await user.save();
   res.status(200).json({ message: "회원가입 성공! 로그인 해주세요." });
+};
+
+exports.signin = (req, res) => {
+  // find the user based on email
+  const { _id, name, email, password } = req.body;
+  // if error or no user
+  // if user, authenticate
+  // generate a token with user id and secret
+  // persist the token as 't' in cookie with expiry date
+  // return response with user and token to frontend client
 };
