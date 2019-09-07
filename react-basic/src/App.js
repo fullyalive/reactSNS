e588d1d731aa2c32866288fbae0e118d3d97ba55
fullyalive <wogud1202@gmail.com>
2019-09-07 23:37:29 +0900

    reactBasic | Rendering state data using map

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -10,16 +10,33 @@ class App extends Component {
     };
   }
 
-  componentWillMount() {
+  getUsers() {
     axios("https://api.randomuser.me/?nat=US&results=5").then(response =>
       this.setState({
         users: response.data.results
       })
     );
+  }
+
+  componentWillMount() {
+    this.getUsers();
   } // Component가 mount되기 전에 API를 부르고 싶을 때 사용
 
   render() {
-    return <div className="App">Hello, fullyalive</div>;
+    return (
+      <div className="App">
+        {this.state.users.map(user => (
+          <div>
+            {user.name.first} {user.name.last}
+            <hr />
+            <ul>
+              <li>{user.email}</li>
+              <li>{user.cell}</li>
+            </ul>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
 
