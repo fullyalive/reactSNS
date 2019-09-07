62ad3620fbe364cdbf0dd7842c51d6bf9314c792
fullyalive <wogud1202@gmail.com>
2019-09-08 00:19:24 +0900

    frontend | Using react router dom

--- react-front/src/App.js
+++ react-front/src/App.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
+import MainRouter from "./MainRouter";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>React Frontend</h1>
-      </div>
-    )
-  }
-}
+const App = () => (
+  <BrowserRouter>
+    <MainRouter />
+  </BrowserRouter>
+);
 
 export default App;
