a4928c10284f24576a8a523f7573b8475ed8b5f3
fullyalive <wogud1202@gmail.com>
2019-09-08 01:12:59 +0900

    frontend | Handling onChange events

--- react-front/src/user/Signup.js
+++ react-front/src/user/Signup.js
@@ -10,22 +10,40 @@ class Signup extends Component {
       error: ""
     };
   }
+
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.value });
+  };
+
   render() {
+    const { name, email, password } = this.state;
     return (
       <div>
         <h2>회원가입</h2>
         <form>
           <div>
             <label>이름</label>
-            <input type="text"></input>
+            <input
+              onChange={this.handleChange("name")}
+              type="text"
+              value={name}
+            ></input>
           </div>
           <div>
             <label>이메일</label>
-            <input type="email"></input>
+            <input
+              onChange={this.handleChange("email")}
+              type="email"
+              value={email}
+            ></input>
           </div>
           <div>
             <label>비밀번호</label>
-            <input type="password"></input>
+            <input
+              onChange={this.handleChange("password")}
+              type="password"
+              value={password}
+            ></input>
           </div>
           <button>가입하기</button>
         </form>
