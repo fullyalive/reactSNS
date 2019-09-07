b81f5b5dccf621df8bff4f82c0dbb197e1337396
fullyalive <wogud1202@gmail.com>
2019-09-07 13:41:01 +0900

    ECMA6 | Restructuring Object

--- ECMA6/index.js
+++ ECMA6/index.js
@@ -1,3 +1,22 @@
+// Restructuring Object
+
+var name = "Everest";
+var height = 8848;
+var output = function() {
+  console.log(`Mt.${this.name} is ${this.height} meter tall`);
+};
+var adventureClimbing = { name, height, output };
+adventureClimbing.output();
+
+var adventureClimbing2 = {
+  name: "Everest",
+  height: 8848,
+  output() {
+    console.log(`Mt.${this.name} is ${this.height} meter tall`);
+  }
+};
+adventureClimbing2.output();
+
 // Destructuring Object
 
 let thingsToDo = {
