ca1417c3cf46022a0d59524d7e5fc5d92ad7632b
fullyalive <wogud1202@gmail.com>
2019-09-08 00:43:10 +0900

    frontend | Signup form

--- react-front/src/user/Signup.js
+++ react-front/src/user/Signup.js
@@ -1,10 +1,34 @@
 import React, { Component } from "react";
 
 class Signup extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+      error: ""
+    };
+  }
   render() {
     return (
       <div>
-        <h2>Sign up</h2>
+        <h2>회원가입</h2>
+        <form>
+          <div>
+            <label>이름</label>
+            <input type="text"></input>
+          </div>
+          <div>
+            <label>이메일</label>
+            <input type="email"></input>
+          </div>
+          <div>
+            <label>비밀번호</label>
+            <input type="password"></input>
+          </div>
+          <button>가입하기</button>
+        </form>
       </div>
     );
   }
