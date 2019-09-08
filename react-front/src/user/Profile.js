d81cea0cf6d4ed2804f3c534f9ad95aedb31297f
fullyalive <wogud1202@gmail.com>
2019-09-09 00:40:29 +0900

    frontend | Showing user info from local storage

--- react-front/src/user/Profile.js
+++ react-front/src/user/Profile.js
@@ -1,10 +1,25 @@
 import React, { Component } from "react";
+import { isAuthenticated } from "../auth";
 
 class Profile extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: "",
+      redirectToSignin: false
+    };
+  }
+
+  componentDidMount() {
+    console.log("user id from route params: ", this.props.match.params.userId);
+  }
+
   render() {
     return (
       <div>
         <h2>내정보</h2>
+        <p>{isAuthenticated().user.name}</p>
+        <p>{isAuthenticated().user.email}</p>
       </div>
     );
   }
