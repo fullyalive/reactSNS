eea7ac28ad22806c20f884e040b934eafde9b058
fullyalive <wogud1202@gmail.com>
2019-09-08 17:36:45 +0900

    frontend | Active link using withRouter

--- react-front/src/core/Menu.js
+++ react-front/src/core/Menu.js
@@ -1,14 +1,26 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 
-const Menu = () => {
+const isActive = (history, path) => {
+  if (history.location.pathname === path) {
+    return { color: "#ff9900" };
+  } else return { color: "#373737" };
+};
+
+const Menu = ({ history }) => {
   return (
     <div>
-      <Link to="/">홈</Link>
-      <Link to="/signin">로그인</Link>
-      <Link to="/signup">회원가입</Link>
+      <Link style={isActive(history, "/")} to="/">
+        홈
+      </Link>
+      <Link style={isActive(history, "/signin")} to="/signin">
+        로그인
+      </Link>
+      <Link style={isActive(history, "/signup")} to="/signup">
+        회원가입
+      </Link>
     </div>
   );
 };
 
-export default Menu;
+export default withRouter(Menu);
