c42b499b45863bf86738b502d0ac6cf935fecad0
fullyalive <wogud1202@gmail.com>
2019-09-09 21:38:41 +0900

    frontend | Fetch user profile

--- react-front/src/user/Profile.js
+++ react-front/src/user/Profile.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { isAuthenticated } from "../auth";
+import { Redirect } from "react-router-dom";
 
 class Profile extends Component {
   constructor() {
@@ -11,15 +12,36 @@ class Profile extends Component {
   }
 
   componentDidMount() {
-    console.log("user id from route params: ", this.props.match.params.userId);
+    const userId = this.props.match.params.userId;
+    fetch(`${process.env.REACT_APP_API_URL}/user/${userId}`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${isAuthenticated().token}`
+      }
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        if (data.error) {
+          this.setState({ redirectToSignin: true });
+        } else {
+          this.setState({ user: data });
+        }
+      });
   }
 
   render() {
+    const redirectToSignin = this.state.redirectToSignin;
+    if (redirectToSignin) return <Redirect to="/signin" />;
     return (
       <div>
         <h2>내정보</h2>
         <p>{isAuthenticated().user.name}</p>
         <p>{isAuthenticated().user.email}</p>
+        <p>{`가입일 ${new Date(this.state.user.created).toDateString()}`}</p>
       </div>
     );
   }
