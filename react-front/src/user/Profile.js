bf42aa10fe283949e49595ec265bcd71a6264eeb
fullyalive <wogud1202@gmail.com>
2019-09-12 02:01:32 +0900

    frontend | User profile based on props change

--- react-front/src/user/Profile.js
+++ react-front/src/user/Profile.js
@@ -30,6 +30,11 @@ class Profile extends Component {
     this.init(userId);
   }
 
+  componentWillReceiveProps(props) {
+    const userId = props.match.params.userId;
+    this.init(userId);
+  }
+
   render() {
     const { redirectToSignin, user } = this.state;
     if (redirectToSignin) return <Redirect to="/signin" />;
