94f95ea5dfba11d511967c6388f8e606c200f567
fullyalive <wogud1202@gmail.com>
2019-09-14 22:25:00 +0900

    frontend | Update profile with image

--- nodeapi/models/user.js
+++ nodeapi/models/user.js
@@ -13,6 +13,10 @@ const userSchema = new mongoose.Schema({
     trim: true,
     required: true
   },
+  photo: {
+    data: Buffer,
+    contentType: String
+  },
   hashed_password: {
     type: String,
     required: true
