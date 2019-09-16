48a982db7ba3a7981174ec4a5f48b99faf4d9183
fullyalive <wogud1202@gmail.com>
2019-09-16 22:08:57 +0900

    nodeAPI | Following and followers - Routes and controller methods

--- nodeapi/routes/user.js
+++ nodeapi/routes/user.js
@@ -5,11 +5,15 @@ const {
   getUser,
   updateUser,
   deleteUser,
-  userPhoto
+  userPhoto,
+  addFollowing,
+  addFollower
 } = require("../controllers/user");
 const { requireSignin } = require("../controllers/auth");
 
 const router = express.Router();
+
+router.put("/user/follow", requireSignin, addFollowing, addFollower);
 
 router.get("/users", allUsers);
 router.get("/user/:userId", requireSignin, getUser);
@@ -17,7 +21,7 @@ router.put("/user/:userId/", requireSignin, updateUser);
 router.delete("/user/:userId/", requireSignin, deleteUser);
 
 // photo
-router.get("/user/photo/:userId", userPhoto)
+router.get("/user/photo/:userId", userPhoto);
 // any route containing : userId, our app will first execute userById()
 router.param("userId", userById);
 
