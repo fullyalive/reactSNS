ce735b5b52bbb3e5266b2a244f4dded36339089f
fullyalive <wogud1202@gmail.com>
2019-09-16 21:40:59 +0900

    frontend | Following and followers - User schema and userById method

--- nodeapi/controllers/user.js
+++ nodeapi/controllers/user.js
@@ -4,15 +4,19 @@ const formidable = require("formidable");
 const fs = require("fs");
 
 exports.userById = (req, res, next, id) => {
-  User.findById(id).exec((err, user) => {
-    if (err || !user) {
-      return res.status(400).json({
-        error: "찾을 수 없는 사용자입니다."
-      });
-    }
-    req.profile = user; // adds profile object in req with user info
-    next();
-  });
+  User.findById(id)
+    // populate followers and following users array
+    .populate("following", "_id name")
+    .populate("followers", "_id name")
+    .exec((err, user) => {
+      if (err || !user) {
+        return res.status(400).json({
+          error: "찾을 수 없는 사용자입니다."
+        });
+      }
+      req.profile = user; // adds profile object in req with user info
+      next();
+    });
 };
 
 exports.hasAuthorization = (req, res, next) => {
--- nodeapi/models/user.js
+++ nodeapi/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const uuidv1 = require("uuid/v1");
 const crypto = require("crypto");
+const { ObjectId } = mongoose.Schema;
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -21,6 +22,8 @@ const userSchema = new mongoose.Schema({
     type: String,
     trim: true
   },
+  following: [{ type: ObjectId, ref: "User" }],
+  followers: [{ type: ObjectId, ref: "User" }],
   hashed_password: {
     type: String,
     required: true
