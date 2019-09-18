cd0fd1f5165549c92e05fb9719bd60c2c498feaf
fullyalive <wogud1202@gmail.com>
2019-09-18 21:41:16 +0900

    frontend | Display followers list

--- react-front/src/user/ProfileTabs.js
+++ react-front/src/user/ProfileTabs.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Avatar from "../images/avatar.png";
 
 class ProfileTabs extends Component {
   render() {
@@ -11,7 +13,24 @@ class ProfileTabs extends Component {
         </div>
         <div>
           followers
-          {JSON.stringify(followers)}
+          {followers.map((person, i) => {
+            return (
+              <div key={i}>
+                <div>
+                  <Link to={`/user/${person._id}`}>
+                    <img
+                      onError={i => {
+                        i.target.src = `${Avatar}`;
+                      }}
+                      src={`${process.env.REACT_APP_API_URL}/user/photo/${person._id}`}
+                      alt={person.name}
+                    />
+                    {person.name}
+                  </Link>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     );
