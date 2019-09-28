e7a5c58e8bd5082c9d64cd507d6335297133f95d
fullyalive <wogud1202@gmail.com>
2019-09-28 14:04:00 +0900

    frontend | Display single post

--- react-front/src/post/SinglePost.js
+++ react-front/src/post/SinglePost.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { singlePost } from "./apiPost";
+// import DefaultPost from "../images/defaultPost.png";
+import { Link } from "react-router-dom";
 
 class SinglePost extends Component {
   state = {
@@ -17,11 +19,33 @@ class SinglePost extends Component {
     });
   };
 
-  render() {
+  renderPost = post => {
+    const posterId = post.postedBy ? `/user/${post.postedBy._id}` : "";
+    const posterName = post.postedBy ? post.postedBy.name : " Unknown";
     return (
       <div>
-        <h2>Single post</h2>
-        {JSON.stringify(this.state.post)}
+        <p>{post.body}</p>
+        <img
+          src={`${process.env.REACT_API_URL}/post/photo/${post._id}`}
+          alt={post.title}
+          // onError={i => i.target.src = `${DefaultPost}`}
+        />
+        <br />
+        <p>
+          by <Link to={posterId}>{posterName}</Link>
+        </p>
+        <Link to={`/`}>목록</Link>
+        on {new Date(post.created).toDateString()}
+      </div>
+    );
+  };
+
+  render() {
+    const { post } = this.state;
+    return (
+      <div>
+        <h2>{post.title}</h2>
+        {this.renderPost(post)}
       </div>
     );
   }
